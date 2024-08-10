0ec3d688cd37aff44417fcd38cced2bf52a757a7
mhmdksh <eddsnx3@gmail.com>
2024-08-11 02:35:58 +0300

    full refactor to work with our envs

---
+++ index.js
@@ -0,0 +1,38 @@
+const { calculateTokenSupplyCG } = require('./src/coingecko'); // Import the function from coingecko.js
+const { calculateTokenSupplyCMC } = require('./src/coinmarketcap'); // Import the function from coinmarketcap.js
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route to serve the JSON response with a 200 status code for CoinMarketCap
+app.get('/giv-supply-cmc', async (req, res) => {
+    try {
+        const queryParam = req.query.q;
+        if (!queryParam) {
+            return res.status(400).json({ error: "Invalid input. Missing query parameter." });
+        }
+        const result = await calculateTokenSupplyCMC(queryParam);
+        if (result === "invalid input") {
+            return res.status(400).json({ error: "Invalid input. Unsupported query parameter." });
+        }
+        res.status(200).json({ [queryParam]: result.toString() });
+    } catch (error) {
+        res.status(500).json({ error: 'Error loading data' });
+    }
+});
+
+// Route to serve the JSON response with a 200 status code for CoinGecko
+app.get('/giv-supply-cg', async (req, res) => {
+    try {
+        const result = await calculateTokenSupplyCG();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Error loading data' });
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ src/coingecko.js
@@ -0,0 +1,69 @@
+const ethers = require("ethers");
+const urlxDAIProvider = "https://rpc.gnosischain.com";
+const urlMainetProvider = "https://rpc.ankr.com/eth";
+const urlOptimismProvider = "https://mainnet.optimism.io";
+const TokenArtifact = require("./abi/GIV.json");
+const BridgedTokenArtifact = require("./abi/GIV-Bridged-L2.json");
+const Token = "0x900db999074d9277c5da2a43f252d74366230da0";
+const Token_xDAI = "0x4f4F9b8D5B4d0Dc10506e5551B0513B61fD59e75";
+const Token_optimism = '0x528CDc92eAB044E1E39FE43B9514bfdAB4412B98';
+
+const mainnet_sc = [
+  "0x87dE995F6744B75bBe0255A973081142aDb61f4d", // Token Distro
+  "0xf924fF0f192f0c7c073161e0d62CE7635114e74f", // Liquidity Safe
+  "0x2B0ee142dCFE7C2dD150cDbd7B6832F6e9977f51", // OneGIV Liquidity Multisig
+];
+
+const xdai_sc = [
+  "0xc0dbDcA66a0636236fAbe1B3C16B1bD4C84bB1E1", // Token Distro
+  "0xe70494225312c6b1167c15134dab66730f36708d", // Giveth Treasury
+  "0xf4ef9a155dd268e321afa7b9391e0602a7b09588", // GIVgarden Common Pool
+  "0xf924fF0f192f0c7c073161e0d62CE7635114e74f", // Liquidity Safe
+  "0xd10BAC02a02747cB293972f99981F4Faf78E1626", // GIVgarden Multisig Wallet
+  "0x0018C6413BFE5430ff9ba4bD7ac3B6AA89BEBD9b", // nrGIV multisig
+];
+
+const optimism_sc = [
+  '0xE3Ac7b3e6B4065f4765d76fDC215606483BF3bD1', // Token Distro
+  '0xf924fF0f192f0c7c073161e0d62CE7635114e74f', // Liquidity Safe
+  "0x0018C6413BFE5430ff9ba4bD7ac3B6AA89BEBD9b", // nrGIV multisig
+];
+
+async function calculateTokenSupplyCG() {
+  const mainnetProvider = new ethers.providers.JsonRpcProvider(urlMainetProvider);
+  const xDAIProvider = new ethers.providers.JsonRpcProvider(urlxDAIProvider);
+  const optimismProvider = new ethers.providers.JsonRpcProvider(urlOptimismProvider);
+
+  const token = new ethers.Contract(Token, TokenArtifact.abi, mainnetProvider);
+  const token_xdai = new ethers.Contract(Token_xDAI, TokenArtifact.abi, xDAIProvider);
+  const token_optimism = new ethers.Contract(Token_optimism, BridgedTokenArtifact, optimismProvider);
+
+  const totalSupply = await token.totalSupply();
+  let circulating = totalSupply;
+
+  const mainnet_sc_promises = mainnet_sc.map(item => token.balanceOf(item));
+  const xdai_sc_promises = xdai_sc.map(item => token_xdai.balanceOf(item));
+  const optimism_sc_promises = optimism_sc.map(item => token_optimism.balanceOf(item));
+
+  const mainnet_values = await Promise.all(mainnet_sc_promises);
+  mainnet_values.forEach(value => {
+    circulating = circulating.sub(value);
+  });
+
+  const xdai_values = await Promise.all(xdai_sc_promises);
+  xdai_values.forEach(value => {
+    circulating = circulating.sub(value);
+  });
+
+  const optimism_values = await Promise.all(optimism_sc_promises);
+  optimism_values.forEach(value => {
+    circulating = circulating.sub(value);
+  });
+
+  return {
+    totalSupply: totalSupply.toString(),
+    circulating: circulating.toString(),
+  };
+}
+
+module.exports = { calculateTokenSupplyCG };
